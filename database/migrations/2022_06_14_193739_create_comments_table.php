505ec6af9d130312966707e42569ec2ed57095ad
Hector Luis Barrientos Margolles <hbarrientos@eshopworld.com>
2022-06-17 18:20:20 +0200

    Add DB structure

---
+++ database/migrations/2022_06_14_193739_create_comments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('post_id')
+                ->references('id')->on('posts')
+                ->onDelete('cascade');
+            $table->foreign('author_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->text('content');
+            $table->boolean('is_published')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
