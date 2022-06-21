ec2c24b0eb163d579bc9f43331e874f8650089f2
Hector Luis Barrientos Margolles <hbarrientos@eshopworld.com>
2022-06-21 08:03:37 +0200

    Small code refactor

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+const SLUG_CONSTRAINT = '[A-Za-z0-9-_]+';
+const ID_CONSTRAINT = '[0-9]+';
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,45 +17,79 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Auth::routes();
-
-Route::get('/', [App\Http\Controllers\PostController::class, 'index']);
-
+Route::get('/', [
+    PostController::class,
+    'index',
+]);
 Route::group(['prefix' => 'auth'], function () {
     Auth::routes();
 });
-
-// check for logged in user
+// Logged in user
 Route::middleware(['auth'])->group(function () {
-    // show new post form
-    Route::get('new-post', [App\Http\Controllers\PostController::class, 'create']);
-    // save new post
-    Route::post('new-post', [App\Http\Controllers\PostController::class, 'save']);
-    // edit post form
-    Route::get('edit/{slug}', [App\Http\Controllers\PostController::class, 'edit']);
-    // update post
-    Route::post('update', [App\Http\Controllers\PostController::class, 'update']);
-    // delete post
-    Route::get('delete/{id}', [App\Http\Controllers\PostController::class, 'destroy']);
-    // display user's all posts
-    Route::get('my-all-posts', [App\Http\Controllers\UserController::class, 'all']);
-    // display user's drafts
-    Route::get('user/{id}/my-drafts', [App\Http\Controllers\UserController::class, 'draft']);
-    // add comment
-    Route::post('comment/add', [App\Http\Controllers\CommentController::class, 'save']);
-    // delete comment
-    Route::post('comment/delete/{id}', [App\Http\Controllers\CommentController::class, 'destroy']);
-    // User information
-    Route::get('user/{id}', [App\Http\Controllers\UserController::class, 'profile'])->where('id', '[0-9]+');
-
+    // Show new post form
+    Route::get('new-post', [
+        PostController::class,
+        'create',
+    ]);
+    // Save new post
+    Route::post('new-post', [
+        PostController::class,
+        'save',
+    ]);
+    // Edit post form
+    Route::get('edit/{slug}', [
+        PostController::class,
+        'edit',
+    ]);
+    // Update post
+    Route::post('update', [
+        PostController::class,
+        'update',
+    ]);
+    // Delete post
+    Route::get('delete/{id}', [
+        PostController::class,
+        'destroy',
+    ]);
+    // Show all posts
+    Route::get('my-all-posts', [
+        UserController::class,
+        'all',
+    ]);
+    // Show drafts
+    Route::get('user/{id}/my-drafts', [
+        UserController::class,
+        'draft',
+    ]);
+    // Add new comment
+    Route::post('comment/add', [
+        CommentController::class,
+        'save',
+    ]);
+    // Delete comment
+    Route::post('comment/delete/{id}', [
+        CommentController::class,
+        'destroy',
+    ]);
+    // User profile
+    Route::get('user/{id}', [
+        UserController::class,
+        'profile',
+    ])->where('id', ID_CONSTRAINT);
     // Logout
-    Route::get('auth/logout', [App\Http\Controllers\LogoutController::class, 'execute']);
-
+    Route::get('auth/logout', [
+        App\Http\Controllers\LogoutController::class,
+        'execute',
+    ]);
 });
-
-// display list of posts
-Route::get('user/{id}/posts', [App\Http\Controllers\UserController::class, 'published'])->where('id', '[0-9]+');
-
-// display single post
-Route::get('/{slug}', [App\Http\Controllers\PostController::class, 'show'])->where('slug', '[A-Za-z0-9-_]+');
+// Show list of posts
+Route::get('user/{id}/posts', [
+    UserController::class,
+    'published',
+])->where('id', ID_CONSTRAINT);
+// Show single post
+Route::get('/{slug}', [
+    PostController::class,
+    'show',
+])->where('slug', SLUG_CONSTRAINT);
